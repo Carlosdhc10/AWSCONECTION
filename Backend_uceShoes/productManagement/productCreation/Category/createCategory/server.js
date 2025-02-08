257ec8a6bd63b623eedef443763f82891f380171
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-02-08 04:29:19 -0500

    feat:

--- Backend_uceShoes/productManagement/productCreation/Category/createCategory/server.js
+++ Backend_uceShoes/productManagement/productCreation/Category/createCategory/server.js
@@ -8,7 +8,7 @@ const  sequelize  = require('./config/database');
 const Category = require('./models/Category');
 const PORT = process.env.PORT || 3005;
 
-// Middleware test creation category 06
+// Middleware test creation category 07
 app.use(cors());
 app.use(express.json());
 
